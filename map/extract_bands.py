876030d08ea50b4d3cc5cf9cca1821ffac6d61f5
dgketchum <dgketchum@gmail.com>
2018-10-19 18:21:45 -0600

    eport ran

--- map/extract_bands.py
+++ map/extract_bands.py
@@ -19,12 +19,13 @@ import ee
 from map.distribute_points import get_years
 
 YEARS = get_years()
-TABLE = 'users/dgketchum/irrigation_projects/MT_Statewide_Irr'
 ROI = 'users/dgketchum/boundaries/western_states_polygon'
-PLOTS = 'users/dgketchum/classify/sample_4c_45k'
+PLOTS = 'ft:1zzupnYQCNoxnOpVcvjEDCVEpdajslg4xz33KDm3U'
 
 
 def request_band_extract():
+    roi = ee.FeatureCollection(ROI)
+    plots = ee.FeatureCollection(PLOTS)
     for yr in YEARS:
         start = '{}-01-01'.format(yr)
         end_date = '{}-01-01'.format(yr + 1)
@@ -33,13 +34,13 @@ def request_band_extract():
         fall_s = '{}-09-01'.format(yr)
 
         l5_coll = ee.ImageCollection('LANDSAT/LT05/C01/T1_SR').filterBounds(
-            ROI).filterDate(start, end_date).map(ls5_edge_removal).map(ls57mask)
+            roi).filterDate(start, end_date).map(ls5_edge_removal).map(ls57mask)
 
         l7_coll = ee.ImageCollection('LANDSAT/LE07/C01/T1_SR').filterBounds(
-            ROI).filterDate(start, end_date).map(ls57mask)
+            roi).filterDate(start, end_date).map(ls57mask)
 
         l8_coll = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR').filterBounds(
-            ROI).filterDate(start, end_date).map(ls8mask)
+            roi).filterDate(start, end_date).map(ls8mask)
 
         lsSR_masked = ee.ImageCollection(l7_coll.merge(l8_coll).merge(l5_coll))
         lsSR_spr_mn = ee.Image(lsSR_masked.filterDate(spring_s, summer_s).mean())
@@ -47,7 +48,7 @@ def request_band_extract():
         lsSR_fal_mn = ee.Image(lsSR_masked.filterDate(fall_s, end_date).mean())
 
         gridmet = ee.ImageCollection("IDAHO_EPSCOR/GRIDMET").filterBounds(
-            ROI).filterDate(start, end_date).select('pr', 'eto', 'tmmn', 'tmmx').map(lambda x: x)
+            roi).filterDate(start, end_date).select('pr', 'eto', 'tmmn', 'tmmx').map(lambda x: x)
 
         temp_reducer = ee.Reducer.percentile([10, 50, 90])
         t_names = ['tmmn_p10_cy', 'tmmn_p50_cy', 'tmmn_p90_cy', 'tmmx_p10_cy', 'tmmx_p50_cy', 'tmmx_p90_cy']
@@ -66,16 +67,16 @@ def request_band_extract():
         input_bands = input_bands.addBands(static_input_bands)
 
         plot_sample_regions = input_bands.sampleRegions(
-            collection=PLOTS.filter(ee.Filter.eq('YEAR', ee.Date(start).get('YEAR'))),
+            collection=plots.filter(ee.Filter.eq('YEAR', ee.Date(start).get('YEAR'))),
             properties=['POINT_TYPE', 'YEAR'],
             scale=30,
             tileScale=16)
 
         task = ee.batch.Export.table.toCloudStorage(
             plot_sample_regions,
-            description='{}_{}'.format(PLOTS.getInfo()['id'].split('/')[-1], yr),
+            description='{}'.format(yr),
             bucket='wudr',
-            fileNamePrefix='{}_{}'.format(PLOTS.getInfo()['id'].split('/')[-1], yr),
+            fileNamePrefix='{}'.format(yr),
             fileFormat='CSV')
 
         task.start()
@@ -103,7 +104,7 @@ def ls57mask(img):
     img_nsat = sr_bands.updateMask(mask_sat)
     mask1 = img.select('pixel_qa').bitwiseAnd(8).eq(0)
     mask2 = img.select('pixel_qa').bitwiseAnd(32).eq(0)
-    mask_p = (mask1 & mask2)
+    mask_p = mask1.And(mask2)
     img_masked = img_nsat.updateMask(mask_p)
     mask_sel = img_masked.select(['B1', 'B2', 'B3', 'B4', 'B5', 'B7'], ['B2', 'B3', 'B4', 'B5', 'B6', 'B7'])
     mask_mult = mask_sel.multiply(0.0001).copyProperties(img, ['system:time_start'])
@@ -116,7 +117,7 @@ def ls8mask(img):
     img_nsat = sr_bands.updateMask(mask_sat)
     mask1 = img.select('pixel_qa').bitwiseAnd(8).eq(0)
     mask2 = img.select('pixel_qa').bitwiseAnd(32).eq(0)
-    mask_p = (mask1 & mask2)
+    mask_p = mask1.And(mask2)
     img_masked = img_nsat.updateMask(mask_p)
     mask_mult = img_masked.multiply(0.0001).copyProperties(img, ['system:time_start'])
     return mask_mult
