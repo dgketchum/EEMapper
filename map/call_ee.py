788d220a17f6d4bc08e62b31d495d0e64e8ada92
dgketchum <dgketchum@gmail.com>
2018-12-12 16:57:39 -0700

    did want ID rerun

--- map/call_ee.py
+++ map/call_ee.py
@@ -32,7 +32,7 @@ ASSET_ROOT = 'users/dgketchum/classy'
 STATES = ['AZ', 'CA', 'CO', 'ID', 'KS', 'MT', 'ND', 'NE',
           'NM', 'NV', 'OK', 'OR', 'SD', 'TX', 'UT', 'WA', 'WY']
 
-EDIT_STATES = ['CO', 'KS', 'ND', 'NE', 'OK', 'SD', 'TX']
+EDIT_STATES = ['ID']  # 'CO', 'KS', 'ND', 'NE', 'OK', 'SD', 'TX']
 
 POINTS = 'ft:1Ai9IqHeW4vhZfLP_F6T9vE7N6Gcppx4-WDctiYGi'
 TABLE = 'ft:1xSWqNQ2P_Og3TwSsp1semWMu88n-I3_kc7Cu4Drq'
@@ -371,7 +371,7 @@ if __name__ == '__main__':
     is_authorized()
     # request_band_extract('bands_11DEC')
     # filter_irrigated()
-    # for state in STATES:
-    #     bounds = os.path.join(BOUNDARIES, state)
-    #     export_classification(out_name='{}'.format(state), asset=bounds, export='asset')
+    for state in EDIT_STATES:
+        bounds = os.path.join(BOUNDARIES, state)
+        export_classification(out_name='{}'.format(state), asset=bounds, export='asset')
 # ========================= EOF ====================================================================
