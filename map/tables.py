2c570b869cd712868b1b857fc1519172762a4a04
dgketchum <dgketchum@gmail.com>
2019-01-16 11:50:32 -0700

    multi-geometries were caused by clipping the HUC boundaries to the Western States Polygon

--- map/tables.py
+++ map/tables.py
@@ -17,7 +17,7 @@
 import json
 import os
 
-from geopandas import GeoDataFrame
+from geopandas import GeoDataFrame, read_file
 from numpy import where, array, sum, nan
 from pandas import read_csv, concat, errors, Series
 from pandas.io.json import json_normalize
@@ -28,6 +28,9 @@ INT_COLS = ['POINT_TYPE', 'YEAR']
 
 KML_JUNK = ['Name', 'descriptio', 'timestamp', 'begin', 'end', 'altitudeMo',
             'tessellate', 'extrude', 'visibility', 'drawOrder', 'icon']
+
+KML_TIME_SERIES_KEEP = ['areaacres', 'areasqkm', 'huc6', 'loaddate', 'Mean_1986',
+                        'shape_area', 'shape_leng', 'states', 'Count_1986', 'geometry']
 
 
 def concatenate_band_extract(root, out_dir, glob='None', sample=None):
@@ -111,37 +114,55 @@ def concatenate_irrigation_attrs(_dir, out_filename):
     gpd.to_file(out_filename)
 
 
-def concatenate_sum_attrs(_dir, out_filename):
+def concatenate_sum_attrs(_dir, out_filename, template_geometry=None):
+
     _files = [os.path.join(_dir, x) for x in os.listdir(_dir) if x.endswith('.csv')]
     _files.sort()
     first = True
-    for year in range(1986, 2017):
+
+    for year in range(1986, 1987):
         print(year)
+        df_geo = None
         yr_files = [f for f in _files if str(year) in f]
         _mean = [f for f in yr_files if 'mean' in f][0]
         _count = [f for f in yr_files if 'count' in f][0]
+
         if first:
             df = read_csv(_mean, index_col=0)
-            df_geo = df['.geo']
+            names = df['Name']
+
+            if template_geometry:
+                template_gpd = read_file(template_geometry)
+                str_geo = template_gpd['geometry']
+
+
+            else:
+                str_geo = df['.geo']
+                coords = Series(json_normalize(str_geo.apply(json.loads))['coordinates'].values,
+                                index=df.index)
+                df_geo = coords.apply(to_polygon)
+
             df.drop(columns=['.geo'], inplace=True)
             df.rename(columns={'mean': 'Mean_{}'.format(year)}, inplace=True)
             count_arr = read_csv(_count, index_col=0)['count'].values
             df['Count_{}'.format(year)] = count_arr
             first = False
+
         else:
             mean_arr = read_csv(_mean, index_col=0)['mean'].values
             count_arr = read_csv(_count, index_col=0)['count'].values
             df['Count_{}'.format(year)] = count_arr
             df['Mean_{}'.format(year)] = mean_arr
 
+    df.drop(columns=['Name'], inplace=True)
+    [df.drop(columns=[x], inplace=True) for x in df.columns if x not in KML_TIME_SERIES_KEEP]
+    df['Name'] = names
     # df.to_csv(out_filename)
-    coords = Series(json_normalize(df_geo.apply(json.loads))['coordinates'].values,
-                    index=df.index)
-    df['geometry'] = coords.apply(to_polygon)
-    df.dropna(subset=['geometry'], inplace=True)
-    gpd = GeoDataFrame(df, crs={'init': 'epsg:4326'})
-    geo = gpd['geometry']
-    gpd.to_file(out_filename.replace(os.path.basename(out_filename), 'irrigation_timeseries_huc6.shp'))
+    for i, r in str_geo.iteritems():
+        if not isinstance(r, Polygon):
+            print(type(r), df.iloc[i])
+    gpd = GeoDataFrame(df, crs={'init': 'epsg:4326'}, geometry=template_gpd['geometry'])
+    gpd.to_file(out_filename.replace(os.path.basename(out_filename), '_irrigation_timeseries_huc6.shp'))
 
 
 def to_polygon(j):
@@ -159,7 +180,8 @@ if __name__ == '__main__':
     home = os.path.expanduser('~')
     extracts = os.path.join(home, 'IrrigationGIS', 'time_series', 'exports_huc6')
     out_table = os.path.join(home, 'IrrigationGIS', 'time_series', 'tables', 'concatenated_huc6.csv')
-    concatenate_sum_attrs(extracts, out_table)
+    template = os.path.join(home, 'IrrigationGIS', 'hydrography', 'huc6_semiarid_clip.shp')
+    concatenate_sum_attrs(extracts, out_table, template_geometry=template)
     # csv = os.path.join(extracts, 'concatenated', '')
 
 # ========================= EOF ====================================================================
