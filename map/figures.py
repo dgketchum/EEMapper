df5a5a3a7d6bb4d12b4c1c4e6790e6657aced4cb
dgketchum <dgketchum@gmail.com>
2019-10-24 20:47:50 -0600

    working on subplots for nass vs. irrmapper

--- map/figures.py
+++ map/figures.py
@@ -18,8 +18,8 @@ import os
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid.inset_locator import InsetPosition
+from numpy import array, vstack, append, isnan
 from pandas import read_csv, Series
-from numpy import array, vstack, append, isnan, any
 from sklearn.metrics import r2_score
 
 
@@ -33,41 +33,6 @@ def state_sum(csv):
     s.interpolate(axis=0, inplace=True)
     s.index = s.values
     s.plot(x=s.values, ax=ax, kind='line', lw=1, loglog=True, color='k', style='--', alpha=0.5)
-    ax = df.plot(x='NASS_{}_ac'.format(2002), y='IM{}_ac'.format(2002), kind='scatter', s=4,
-                 xlim=(1e2, 1e6), ylim=(1e2, 1e6), ax=ax, loglog=True, color='g')
-    df.plot(x='NASS_{}_ac'.format(2007), y='IM{}_ac'.format(2007), kind='scatter', s=4,
-            xlim=(1e2, 1e6), ylim=(1e2, 1e6), ax=ax, loglog=True, color='b')
-    df.plot(x='NASS_{}_ac'.format(2012), y='IM{}_ac'.format(2012), kind='scatter', s=4,
-            xlim=(1e2, 1e6), ylim=(1e2, 1e6), ax=ax, loglog=True, color='r')
-    plt.xlabel('NASS FRIS Total Irrigated Acres'.format())
-    plt.ylabel('IrrMapper Total Irrigated Acres'.format())
-    # plt.show()
-    # plt.savefig('figs/state_sum_{}.png'.format(yr))
-    return plt
-
-
-def compare_nass_irrmapper_scatter(csv, fig_name=None, print_correlation=False):
-    df = read_csv(csv)
-    fig, ax = plt.subplots(1, 1)
-    s = Series(index=df.index)
-    s.loc[0], s.loc[df.shape[0]] = 0, 1e6
-    s.interpolate(axis=0, inplace=True)
-    s.index = s.values
-    s.plot(x=s.values, ax=ax, kind='line', lw=1, loglog=True, color='k', style='--', alpha=0.5, label='_nolegend_')
-
-    df.plot(x='NASS_{}_ac'.format(2002), y='IM{}_ac'.format(2002), kind='scatter', s=4,
-                 xlim=(1e2, 1e6), ylim=(1e2, 1e6), ax=ax, loglog=True, color='g')
-    df.plot(x='NASS_{}_ac'.format(2007), y='IM{}_ac'.format(2007), kind='scatter', s=4,
-            xlim=(1e2, 1e6), ylim=(1e2, 1e6), ax=ax, loglog=True, color='b')
-    df.plot(x='NASS_{}_ac'.format(2012), y='IM{}_ac'.format(2012), kind='scatter', s=4,
-            xlim=(1e2, 1e6), ylim=(1e2, 1e6), ax=ax, loglog=True, color='r')
-
-    plt.xlabel('NASS AgStats Total Irrigated Acres, Counties'.format())
-    plt.ylabel('IrrMapper Total Irrigated Acres'.format())
-    legend =ax.legend(['2002', '2007', '2012'], loc='lower right')
-    # fig.suptitle('Lorem ipsum dolor sit amet, consectetur adipiscing elit', ha='center')
-    frame = legend.get_frame()
-    frame.set_color('grey')
     df_state = df.groupby(['State', 'State_Code'])[['IM2002_ac', 'NASS_2002_ac', 'IM2007_ac',
                                                     'NASS_2007_ac', 'IM2012_ac', 'NASS_2012_ac']].sum()
     s = Series(index=df_state.index)
@@ -88,35 +53,79 @@ def compare_nass_irrmapper_scatter(csv, fig_name=None, print_correlation=False):
     s.plot(x=s.values, kind='line', lw=1, loglog=True, color='k', style='--', alpha=0.5, label='_nolegend_')
     ip = InsetPosition(ax, [0.07, 0.67, 0.3, 0.3])
     ax2.set_axes_locator(ip)
-    # plt.show()
-    if fig_name:
-        plt.savefig(fig_name)
+    plt.show()
 
-    if print_correlation:
-        irrmap_cols = [x for x in df_state.columns if 'IM' in x]
-        nass_cols = [x for x in df_state.columns if 'NASS' in x]
-        im_state_df = df_state[irrmap_cols]
-        nass_state_df = df_state[nass_cols]
-        im_state = array([im_state_df[x].values for x in im_state_df]).flatten()
-        nass_state = array([nass_state_df[x].values for x in nass_state_df]).flatten()
-        comp = vstack([im_state, nass_state]).transpose()
-        comp = append(comp, (comp[:, 0] - comp[:, 1]).reshape(comp.shape[0], 1), axis=1)
-        rmse = ((comp[2, :]) ** 2).mean() ** .5
-        coeff_det = r2_score(comp[:, 0], comp[:, 1])
-        print('state r squared: {}'.format(coeff_det))
+    return plt
 
-        irrmap_cols = [x for x in df.columns if 'IM' in x]
-        nass_cols = [x for x in df.columns if 'NASS' in x]
-        im_county_df = df[irrmap_cols]
-        nass_county_df = df[nass_cols]
-        im_county = array([im_county_df[x].values for x in im_county_df]).flatten()
-        nass_county = array([nass_county_df[x].values for x in nass_county_df]).flatten()
-        comp = vstack([im_county, nass_county]).transpose()
-        comp = append(comp, (comp[:, 0] - comp[:, 1]).reshape(comp.shape[0], 1), axis=1)
-        comp = comp[~isnan(comp).any(axis=1)]
-        rmse = ((comp[2, :]) ** 2).mean() ** .5
-        coeff_det = r2_score(comp[:, 0], comp[:, 1])
-        print('county r squared: {}'.format(coeff_det))
+
+def compare_nass_irrmapper_scatter(csv, fig_name):
+    df = read_csv(csv)
+    s = Series(index=df.index)
+    s.loc[0], s.loc[df.shape[0]] = 0, 1e6
+    s.interpolate(axis=0, inplace=True)
+    s.index = s.values
+
+    fig, ax = plt.subplots(2, 4)
+    rows, cols = [0, 0, 0, 0, 1, 1, 1, 1], [0, 1, 2, 3, 0, 1, 2, 3]
+
+    for r, c, year in zip(rows, cols, range(1987, 2022, 5)):
+        n, i = 'NASS_{}'.format(year), 'IM_{}'.format(year)
+        a = ax[r, c]
+        ydf = df[[n, i]]
+
+        s.plot(kind='line', lw=1, loglog=True,
+               color='k', style='--', alpha=0.5, label='_nolegend_', ax=a)
+
+        ydf.plot(n, i, xlim=(1e2, 1e6), ylim=(1e2, 1e6), loglog=True, color='k', alpha=0.3,
+                 kind='scatter', ax=a, marker='o', s=3)
+
+        a.set(adjustable='box')
+        a.set_title(str(year), size=10)
+
+        if c > 0:
+            a.set_yticks([])
+        if r == 0 and c < 3:
+            a.set_xticks([])
+
+        x_axis = a.xaxis
+        x_axis.label.set_visible(False)
+
+        y_axis = a.yaxis
+        y_axis.label.set_visible(False)
+
+    fig.delaxes(ax[1, 3])
+    plt.tight_layout()
+    plt.show()
+    plt.savefig(fig_name)
+
+
+def get_correlations(csv):
+    df_state = read_csv(csv)
+    df = None
+    irrmap_cols = [x for x in df_state.columns if 'IM' in x]
+    nass_cols = [x for x in df_state.columns if 'NASS' in x]
+    im_state_df = df_state[irrmap_cols]
+    nass_state_df = df_state[nass_cols]
+    im_state = array([im_state_df[x].values for x in im_state_df]).flatten()
+    nass_state = array([nass_state_df[x].values for x in nass_state_df]).flatten()
+    comp = vstack([im_state, nass_state]).transpose()
+    comp = append(comp, (comp[:, 0] - comp[:, 1]).reshape(comp.shape[0], 1), axis=1)
+    rmse = ((comp[2, :]) ** 2).mean() ** .5
+    coeff_det = r2_score(comp[:, 0], comp[:, 1])
+    print('state r squared: {}'.format(coeff_det))
+
+    irrmap_cols = [x for x in df.columns if 'IM' in x]
+    nass_cols = [x for x in df.columns if 'NASS' in x]
+    im_county_df = df[irrmap_cols]
+    nass_county_df = df[nass_cols]
+    im_county = array([im_county_df[x].values for x in im_county_df]).flatten()
+    nass_county = array([nass_county_df[x].values for x in nass_county_df]).flatten()
+    comp = vstack([im_county, nass_county]).transpose()
+    comp = append(comp, (comp[:, 0] - comp[:, 1]).reshape(comp.shape[0], 1), axis=1)
+    comp = comp[~isnan(comp).any(axis=1)]
+    rmse = ((comp[2, :]) ** 2).mean() ** .5
+    coeff_det = r2_score(comp[:, 0], comp[:, 1])
+    print('county r squared: {}'.format(coeff_det))
     return None
 
 
@@ -189,27 +198,15 @@ def state_fp_code():
 if __name__ == '__main__':
     home = os.path.expanduser('~')
 
-    tables = os.path.join(home, 'IrrigationGIS', 'time_series')
-    huc_8 = os.path.join(tables, 'tables', 'concatenated_huc8.csv')
-    # time_series_normalized(huc_8)
+    county = os.path.join(home, 'IrrigationGIS', 'time_series', 'exports_county')
+    irr_tables = os.path.join(county, 'counties_v2', 'noCdlMask_minYr5')
 
-    irr_tables = os.path.join(home, 'IrrigationGIS', 'time_series', 'exports_county', 'counties_v2', 'noCdlMask_minYr5')
-    nass_tables = os.path.join(home, 'IrrigationGIS', 'time_series', 'exports_county')
-
-    irr = os.path.join(irr_tables, 'irr_merged.csv')
-    nass = os.path.join(nass_tables, 'nass_merged.csv')
     o = os.path.join(irr_tables, 'nass_irrMap.csv')
-
     irr_all = os.path.join(irr_tables, 'irr_v2_noCdlMask_minYr5_25SEPT2019.csv')
     irr_shp = irr_all.replace('.csv', '.shp')
+    figure = 'figs/irr_totals_timeSeries_24OCT2019.png'
 
-    # figure = 'figs/z_annual_irr_byState_noCdl_5Yr_25SETP2019.png'
-    # figure = 'figs/comparison_noCdl_5Yr_22OCT2019.png'
-    figure = 'figs/irr_totals_timeSeries_22OCT2019.png'
-
-    irr = os.path.join(irr_tables, 'irr_merged_ac.csv')
-
-    compare_nass_irrmapper_scatter(o, print_correlation=True)
+    compare_nass_irrmapper_scatter(o, figure)
     # irr_time_series_states(irr_all, fig_name=figure)
     # state_sum(o)
     # irr_time_series_totals(irr, fig_name=figure)
