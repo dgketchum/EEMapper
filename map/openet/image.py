c41decee7dcaeec31459be7164130534dc711fbd
dgketchum <pdWs7kyy>
2020-07-31 13:09:06 -0600

    testing temporal interp stack extract without cloud mask

--- map/openet/image.py
+++ map/openet/image.py
@@ -302,12 +302,12 @@ class Image():
         ])
 
         # Apply the cloud mask and add properties
-        input_image = input_image\
-            .updateMask(common.landsat_c1_sr_cloud_mask(sr_image))\
+        input_image = input_image \
             .set({'system:index': sr_image.get('system:index'),
                   'system:time_start': sr_image.get('system:time_start'),
                   'system:id': sr_image.get('system:id'),
             })
+        # .updateMask(common.landsat_c1_sr_cloud_mask(sr_image))\
 
         _cls = cls(input_image, **kwargs)
         return _cls
