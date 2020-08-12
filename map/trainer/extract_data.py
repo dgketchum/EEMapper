118f46fdf0485dd7cfdb1ef777af9411ad21931f
dgketchum <pdWs7kyy>
2020-08-12 16:11:16 -0600

    full time depth, 12 shards when asked for 15, moving to attempt extract at points

--- map/trainer/extract_data.py
+++ map/trainer/extract_data.py
@@ -12,7 +12,7 @@ from map.openet.collection import get_target_dates, Collection, get_target_bands
 from map.trainer import extract_utils
 from map.trainer.shapefile_meta import SHP_TO_YEAR_AND_COUNT
 
-KERNEL_SIZE = 64
+KERNEL_SIZE = 256
 KERNEL_SHAPE = [KERNEL_SIZE, KERNEL_SIZE]
 list_ = ee.List.repeat(1, KERNEL_SIZE)
 lists = ee.List.repeat(list_, KERNEL_SIZE)
@@ -28,6 +28,22 @@ COLLECTIONS = ['LANDSAT/LC08/C01/T1_SR',
                'LANDSAT/LT05/C01/T1_SR']
 
 
+def masks():
+    root = 'users/dgketchum/training_polygons/'
+    irr_mask = ee.FeatureCollection(os.path.join(root, 'irrigated_mask'))
+    dryland_mask = ee.FeatureCollection(os.path.join(root, 'dryland_mask'))
+
+    root = 'users/dgketchum/uncultivated/'
+    w_wetlands = ee.FeatureCollection(os.path.join(root, 'west_wetlands'))
+    c_wetlands = ee.FeatureCollection(os.path.join(root, 'central_wetlands'))
+    e_wetlands = ee.FeatureCollection(os.path.join(root, 'east_wetlands'))
+    usgs_pad = ee.FeatureCollection(os.path.join(root, 'usgs_pad'))
+    rf_uncult = ee.FeatureCollection(os.path.join(root, 'west_wetlands'))
+    uncultivated_mask = w_wetlands.merge(c_wetlands).merge(e_wetlands).merge(usgs_pad).merge(rf_uncult)
+
+    return {'irrigated': irr_mask, 'dryland': dryland_mask, 'uncultivated': uncultivated_mask}
+
+
 def test_geo():
     fc = ee.Geometry([[-112.19186229407349, 47.81781050704002],
                       [-112.0552198380188, 47.81781050704002],
@@ -35,31 +51,27 @@ def test_geo():
                       [-112.19186229407349, 47.870346541256765]])
 
 
-def temporally_filter_features(shapefiles, year):
-    shapefile_to_feature_collection = {}
-    for shapefile in shapefiles:
-        temporal_ = True
-        bs = os.path.basename(shapefile)
-        feature_collection = ee.FeatureCollection(shapefile)
-        if 'irrigated' in bs and 'unirrigated' not in bs:
-            feature_collection = feature_collection.filter(ee.Filter.eq("YEAR", year))
-        elif 'fallow' in bs:
-            feature_collection = feature_collection.filter(ee.Filter.eq("YEAR", year))
-        else:
-            temporal_ = False
-            shapefile_to_feature_collection[shapefile] = feature_collection
+def temporally_filter_features(year_):
 
-        if temporal_:
-            shapefile_to_feature_collection[shapefile] = feature_collection
+    shapefile_to_feature_collection = {}
+    for name, fc in masks().items():
+        if 'irrigated' in name:
+            feature_collection = fc.filter(ee.Filter.eq("YEAR", year_))
+            shapefile_to_feature_collection[name] = feature_collection
+
+        elif 'fallow' in name:
+            fc = fc.filter(ee.Filter.eq("YEAR", year_))
+            shapefile_to_feature_collection[name] = fc
+        else:
+            shapefile_to_feature_collection[name] = fc
 
     return shapefile_to_feature_collection
 
 
 def create_class_labels(shapefile_to_feature_collection):
     class_labels = ee.Image(0).byte()
-    for shapefile, feature_collection in shapefile_to_feature_collection.items():
-        class_labels = class_labels.paint(feature_collection,
-                                          assign_class_code(shapefile) + 1)
+    for name, fc in shapefile_to_feature_collection.items():
+        class_labels = class_labels.paint(fc, assign_class_code(name) + 1)
 
     label = class_labels.updateMask(class_labels).rename('irr')
 
@@ -72,9 +84,9 @@ def assign_class_code(shapefile_path):
         return 1
     if 'fallow' in shapefile_path:
         return 2
-    if 'unirrigated' in shapefile_path:
+    if 'dryland' in shapefile_path:
         return 3
-    if 'wetlands' in shapefile_path:
+    if 'uncultivated' in shapefile_path:
         return 4
     if 'points' in shapefile_path:
         # annoying workaround for earthengine
@@ -118,9 +130,7 @@ def get_sr_stack(yr, s, e, interval, geo_):
     return interp, target_rename
 
 
-def extract_data_over_shapefiles(mask_shapefiles, year,
-                                 out_folder, points_to_extract=None,
-                                 n_shards=10):
+def extract_data_over_shapefiles(year, out_folder, points_to_extract=None, n_shards=15):
 
     roi = ee.FeatureCollection(MGRS).filter(ee.Filter.eq('MGRS_TILE', '12TVT')).geometry()
     # roi = ee.FeatureCollection(MT).geometry()
@@ -134,7 +144,7 @@ def extract_data_over_shapefiles(mask_shapefiles, year,
     columns = [tf.io.FixedLenFeature(shape=KERNEL_SHAPE, dtype=tf.float32) for k in features]
     feature_dict = OrderedDict(zip(features, columns))
     # pprint(feature_dict)
-    shapefile_to_feature_collection = temporally_filter_features(mask_shapefiles, year)
+    shapefile_to_feature_collection = temporally_filter_features(year)
     if points_to_extract is not None:
         shapefile_to_feature_collection['points'] = points_to_extract
 
@@ -143,25 +153,17 @@ def extract_data_over_shapefiles(mask_shapefiles, year,
     data_stack = ee.Image.cat([image_stack, terrain_, coords_, irr]).float()
     data_stack = data_stack.neighborhoodToArray(KERNEL)
 
+    feature_count = 0
     if points_to_extract is None:
         for shapefile, feature_collection in shapefile_to_feature_collection.items():
             polygons = feature_collection.toList(feature_collection.size())
-            n_features = SHP_TO_YEAR_AND_COUNT[os.path.basename(shapefile)][year]
             out_class_label = os.path.basename(shapefile)
-            out_filename = out_class_label + "_sr{}{}{}_".format(KERNEL_SIZE, s, e) + str(year)
+            out_filename = out_class_label + "_sr{}_s{}_e{}_".format(KERNEL_SIZE, s, e) + str(year)
             geometry_sample = ee.ImageCollection([])
-            if not n_features:
-                continue
-            n = 10
-            if n_features < n:
-                n = n_features
-            indices = np.random.choice(n_features, size=n)
-            indices = [int(i) for i in indices]
-            print(out_class_label, year, n_features, len(indices))
-            feature_count = 0
-            for i, idx in enumerate(indices):
+
+            for i in range(n_shards):
                 sample = data_stack.sample(
-                    region=ee.Feature(polygons.get(idx)).geometry(),
+                    region=ee.Feature(polygons.get(i)).geometry(),
                     scale=30,
                     numPixels=1,
                     tileScale=8
@@ -188,7 +190,7 @@ def extract_data_over_shapefiles(mask_shapefiles, year,
                         task.start()
                     geometry_sample = ee.ImageCollection([])
             # take care of leftovers
-            print('n_extracted:', feature_count)
+            # print('n_extracted:', feature_count)
             task = ee.batch.Export.table.toCloudStorage(
                 collection=geometry_sample,
                 description=out_filename + str(time.time()),
@@ -242,14 +244,5 @@ def extract_data_over_shapefiles(mask_shapefiles, year,
 
 
 if __name__ == '__main__':
-    root = 'users/dgketchum/training_polygons/'
-    test = ['irrigated_test', 'fallow_test',
-            'unirrigated_test', 'wetlands_test']
-    test = [root + t for t in test]
-    train = ['irrigated_train', 'fallow_train',
-             'unirrigated_train', 'wetlands_train']
-    train = [root + t for t in train]
-
     year = 2010
-
-    extract_data_over_shapefiles(train, year, out_folder=GS_BUCKET)
+    extract_data_over_shapefiles(year, out_folder=GS_BUCKET)
