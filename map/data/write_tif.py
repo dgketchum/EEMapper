4e4d4ef5993497bc02331c8baec68e05e8669bf9
dgketchum <pdWs7kyy>
2020-09-12 09:58:25 -0600

    writes georeferenced gtiff

--- map/data/write_tif.py
+++ map/data/write_tif.py
@@ -18,6 +18,7 @@ import os
 
 import numpy as np
 
+from pyproj import Proj, transform
 import rasterio
 from rasterio.transform import from_origin
 
@@ -32,54 +33,64 @@ r_idx, g_idx, b_idx = [FEATURES.index(x) for x in FEATURES if 'red' in x], \
                       [FEATURES.index(x) for x in FEATURES if 'green' in x], \
                       [FEATURES.index(x) for x in FEATURES if 'blue' in x]
 
+lat_idx = [FEATURES.index(x) for x in FEATURES if 'lat' in x]
+lon_idx = [FEATURES.index(x) for x in FEATURES if 'lon' in x]
+
 from matplotlib import pyplot as plt
 from matplotlib.colors import ListedColormap
 
 cmap = ListedColormap(['grey', 'blue', 'purple', 'pink', 'green'])
 
 
-def build_raster(npy_dir, out_tif_dir):
-    l = [os.path.join(npy_dir, x) for x in os.listdir(npy_dir)]
+def build_raster(npy_dir, out_tif_dir, plot=False):
+    l = [os.path.join(npy_dir, x) for x in os.listdir(npy_dir) if x.endswith('.npy')]
     for j, f in enumerate(l):
 
         a = np.load(f)
         labels = a[:, :, -4:]
-        features = a[:, :, :-4]
+
         for n in range(labels.shape[-1]):
             labels[:, :, n] *= n + 1
         labels = np.sum(labels, axis=-1)
-        # features = features.numpy().squeeze()
 
-        for i, n in enumerate(features):
-            print(i, features[:, :, i].mean())
-
+        features = a[:, :, :-4]
         r, g, b = features[:, :, r_idx], features[:, :, g_idx], features[:, :, b_idx]
 
-        norm = lambda arr: ((arr - arr.min()) * (1 / (arr.max() - arr.min()) * 255)).astype('uint8')
+        norm = lambda arr: ((arr - arr.min()) * (1 / (arr.max() - arr.min()) * 255))
         rgb = map(norm, [np.median(r, axis=2), np.median(g, axis=2), np.median(b, axis=2)])
-        rgb = np.dstack(rgb)
+        rgb = np.dstack(rgb).astype('uint8')
 
-        lat, lon = features[:, :, -3].mean(), features[:, :, -2].mean()
+        lat, lon = features[:, :, lat_idx].max(), features[:, :, lon_idx].min()
+        print(lat, lon)
+        if plot:
+            fig, ax = plt.subplots(ncols=2)
+            ax[0].imshow(rgb)
+            ax[1].imshow(labels, cmap=cmap)
+            plt.suptitle('{:.3f}, {:.3f}'.format(lat, lon))
+            plt.show()
 
-        fig, ax = plt.subplots(ncols=2)
-        ax[0].imshow(rgb)
-        ax[1].imshow(labels, cmap=cmap)
-        plt.suptitle('{:.3f}, {:.3f}'.format(lat, lon))
-        plt.show()
+        in_proj = Proj('epsg:4326')
+        out_proj = Proj('epsg:5070', preserve_units=True)
+        x1, y1 = lon, lat
+        lon, lat = transform(in_proj, out_proj, y1, x1)
 
-        transform = from_origin(472137, 5015782, 0.5, 0.5)
-        new_dataset = rasterio.open('test1.tif', 'w', driver='GTiff',
-                                    height=rgb.shape[0], width=rgb.shape[1],
-                                    count=1, dtype=str(rgb.dtype),
-                                    crs='+proj=utm +zone=10 +ellps=GRS80 +datum=NAD83 +units=m +no_defs',
-                                    transform=transform)
+        affine = from_origin(lon, lat, 30, 30)
+        tif_name = os.path.join(out_tif_dir, '{}.tif'.format(j))
 
-        new_dataset.write(rgb, 1)
-        new_dataset.close()
+        meta = dict(driver='GTiff',
+                    height=rgb.shape[0], width=rgb.shape[1],
+                    count=3, dtype=str(rgb.dtype),
+                    crs='epsg:5070',
+                    transform=affine)
+
+        with rasterio.open(tif_name, 'w', **meta) as dst:
+            for b in range(rgb.shape[-1]):
+                band = rgb[:, :, b].astype(rasterio.uint8)
+                dst.write_band(b + 1, band)
 
 
 if __name__ == '__main__':
     npy = '/home/dgketchum/PycharmProjects/EEMapper/map/data/npy'
     tif = '/home/dgketchum/PycharmProjects/EEMapper/map/data/tif'
-
+    build_raster(npy, tif)
 # ========================= EOF ====================================================================
