37683e44453ebaa1413d66686cd419522f153f89
dgketchum <pdWs7kyy>
2020-10-15 13:09:57 -0600

    move empty files to empty file directory

--- map/data/bucket.py
+++ map/data/bucket.py
@@ -22,10 +22,22 @@ def get_bucket_contents(bucket_name, glob=None):
             dct[dirname].append((b_name, size))
     for k, v in dct.items():
         print(k, sum([x[1] for x in v]))
-    return dct
+    return dct, empty
+
+
+def move_empty(bucket_name, glob=None, dst_folder=None):
+    bucket = client.get_bucket(bucket_name)
+    for blob in bucket.list_blobs(prefix=glob):
+        dirname = os.path.dirname(blob.name)
+        if blob.size == 20:
+            if 'unproc' in dirname:
+                out_name = os.path.join(dst_folder, os.path.basename(blob.name))
+                print('move {} to {}'.format(blob.name, out_name))
+                bucket.rename_blob(blob, out_name)
 
 
 if __name__ == '__main__':
     bucket = 'ts_data'
-    get_bucket_contents(bucket)
+    dst_rt = 'cmask/points/empty'
+    move_empty(bucket, dst_folder=dst_rt)
 # ========================= EOF ====================================================================
