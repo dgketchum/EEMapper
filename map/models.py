48591d5d915b599be73d67061d5016ef7f324782
dgketchum <pdWs7kyy>
2020-10-29 11:39:53 -0600

    export RF tree visualization

--- map/models.py
+++ map/models.py
@@ -1,25 +1,10 @@
-# =============================================================================================
-# Copyright 2018 dgketchum
-#
-# Licensed under the Apache License, Version 2 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# =============================================================================================
-
 import os
 import sys
 from pprint import pprint
 from time import time
+from subprocess import call
 
-import tensorflow as tf
+# import tensorflow as tf
 from numpy import dot, mean, flatnonzero, unique
 from numpy.random import randint
 from pandas import read_csv, concat, get_dummies, DataFrame
@@ -27,16 +12,17 @@ from scipy.stats import randint as sp_randint
 from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix
+from sklearn.tree import export_graphviz
 from sklearn.model_selection import RandomizedSearchCV, GridSearchCV, train_test_split, KFold
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 
 from datetime import datetime
 
-
 abspath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(abspath)
 
 INT_COLS = ['POINT_TYPE', 'YEAR', 'classification']
+CLASS_NAMES = ['IRR', 'DRYL', 'WETl', 'UNCULT']
 
 
 def consumer(arr):
@@ -71,19 +57,14 @@ def pca(csv):
         print(eigenvalue)
 
 
-def random_forest(csv, binary=False):
+def random_forest(csv, binary=False, n_estimators=100):
     df = read_csv(csv, engine='python')
     # df = df.sample(frac=0.5).reset_index(drop=True)
     labels = df['POINT_TYPE'].values
-    print(df['POINT_TYPE'].value_counts())
     df.drop(columns=['YEAR', 'POINT_TYPE'], inplace=True)
     df.dropna(axis=1, inplace=True)
     data = df.values
-
-    print(csv)
     names = df.columns
-    print(list(names))
-    print(df.shape)
     if binary:
         labels = labels.reshape((labels.shape[0],))
         labels[labels > 1] = 1
@@ -93,21 +74,30 @@ def random_forest(csv, binary=False):
     x, x_test, y, y_test = train_test_split(data, labels, test_size=0.33,
                                             random_state=None)
 
-    rf = RandomForestClassifier(n_estimators=100,
+    rf = RandomForestClassifier(n_estimators=n_estimators,
+                                max_features=11,
+                                max_depth=4,
                                 min_samples_split=11,
                                 n_jobs=-1,
                                 bootstrap=False)
 
     rf.fit(x, y)
-    _list = [(f, v) for f, v in zip(names, rf.feature_importances_)]
-    important = sorted(_list, key=lambda x: x[1], reverse=True)
-    pprint(rf.score(x_test, y_test))
-    y_pred = rf.predict(x_test)
-    cf = confusion_matrix(y_test, y_pred)
-    pprint(cf)
-    producer(cf)
-    consumer(cf)
-    return important
+    return rf, names
+
+
+def export_tree(rf, tree_idx, names, out_file=None):
+    tree = rf.estimators_[tree_idx]
+    nodes = tree.tree_.__getstate__()['nodes']
+    feature_idx = [node[2] for node in nodes]
+    feature_names = list(names[feature_idx])
+    export_graphviz(tree, out_file=out_file,
+                    feature_names=feature_names,
+                    class_names=CLASS_NAMES,
+                    rounded=True, proportion=False,
+                    precision=2, filled=True)
+    png = out_file.replace('.dot', '.png')
+    call(['dot', '-Tpng', out_file, '-o', png, '-Gdpi=600'])
+    return None
 
 
 def find_rf_variable_importance(csv):
@@ -197,7 +187,6 @@ def get_size(start_path='.'):
 
 
 def random_hyperparameter_search(csv):
-
     df = read_csv(csv, engine='python')
     labels = df['POINT_TYPE'].values
     df.drop(columns=['YEAR', 'POINT_TYPE'], inplace=True)
@@ -255,7 +244,6 @@ def random_hyperparameter_search(csv):
 
 
 def get_confusion_matrix(csv, spec=None):
-
     df = read_csv(csv, engine='python')
 
     if spec:
@@ -293,85 +281,89 @@ def get_confusion_matrix(csv, spec=None):
     consumer(cf)
 
 
-def mlp(csv):
-    start = datetime.now()
-    df = read_csv(csv, engine='python')
-    labels = df['POINT_TYPE'].values
-    df.drop(columns=['YEAR', 'POINT_TYPE'], inplace=True)
-    data = df.values
-
-    x = normalize_feature_array(data)
-    y = get_dummies(labels.reshape((labels.shape[0],))).values
-    N = len(unique(labels))
-    n = data.data.shape[1]
-    print('')
-    print('train on {}'.format(data.shape))
-
-    nodes = 300
-    eta = 0.01
-    epochs = 10000
-    seed = 128
-    batch_size = 1000
-
-    x, x_test, y, y_test = train_test_split(x, y, test_size=0.33,
-                                            random_state=None)
-
-    X = tf.placeholder("float", [None, n])
-    Y = tf.placeholder("float", [None, N])
-
-    weights = {
-        'hidden': tf.Variable(tf.random_normal([n, nodes], seed=seed), name='Wh'),
-        'output': tf.Variable(tf.random_normal([nodes, N], seed=seed), name='Wo')}
-    biases = {
-        'hidden': tf.Variable(tf.random_normal([nodes], seed=seed), name='Bh'),
-        'output': tf.Variable(tf.random_normal([N], seed=seed), name='Bo')}
-
-    y_pred = tf.add(tf.matmul(multilayer_perceptron(X, weights['hidden'], biases['hidden']),
-                              weights['output']), biases['output'])
-
-    loss_op = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=y_pred, labels=Y))
-
-    optimizer = tf.train.AdamOptimizer(learning_rate=eta).minimize(loss_op)
-
-    init = tf.global_variables_initializer()
-
-    with tf.Session() as sess:
-
-        sess.run(init)
-
-        for step in range(epochs):
-
-            offset = randint(0, y.shape[0] - batch_size - 1)
-
-            batch_data = x[offset:(offset + batch_size), :]
-            batch_labels = y[offset:(offset + batch_size), :]
-
-            feed_dict = {X: batch_data, Y: batch_labels}
-
-            _, loss = sess.run([optimizer, loss_op],
-                               feed_dict=feed_dict)
-
-            if step % 1000 == 0:
-                pred = tf.nn.softmax(y_pred)
-                correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
-                accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-                print('Test accuracy: {}, loss {}'.format(accuracy.eval({X: x_test, Y: y_test}), loss))
-
-    print((datetime.now() - start).seconds)
-    return None
-
-
-def multilayer_perceptron(x, weights, biases):
-    out_layer = tf.add(tf.matmul(x, weights), biases)
-    out_layer = tf.nn.sigmoid(out_layer)
-    return out_layer
+# def mlp(csv):
+#     start = datetime.now()
+#     df = read_csv(csv, engine='python')
+#     labels = df['POINT_TYPE'].values
+#     df.drop(columns=['YEAR', 'POINT_TYPE'], inplace=True)
+#     data = df.values
+#
+#     x = normalize_feature_array(data)
+#     y = get_dummies(labels.reshape((labels.shape[0],))).values
+#     N = len(unique(labels))
+#     n = data.data.shape[1]
+#     print('')
+#     print('train on {}'.format(data.shape))
+#
+#     nodes = 300
+#     eta = 0.01
+#     epochs = 10000
+#     seed = 128
+#     batch_size = 1000
+#
+#     x, x_test, y, y_test = train_test_split(x, y, test_size=0.33,
+#                                             random_state=None)
+#
+#     X = tf.placeholder("float", [None, n])
+#     Y = tf.placeholder("float", [None, N])
+#
+#     weights = {
+#         'hidden': tf.Variable(tf.random_normal([n, nodes], seed=seed), name='Wh'),
+#         'output': tf.Variable(tf.random_normal([nodes, N], seed=seed), name='Wo')}
+#     biases = {
+#         'hidden': tf.Variable(tf.random_normal([nodes], seed=seed), name='Bh'),
+#         'output': tf.Variable(tf.random_normal([N], seed=seed), name='Bo')}
+#
+#     y_pred = tf.add(tf.matmul(multilayer_perceptron(X, weights['hidden'], biases['hidden']),
+#                               weights['output']), biases['output'])
+#
+#     loss_op = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(logits=y_pred, labels=Y))
+#
+#     optimizer = tf.train.AdamOptimizer(learning_rate=eta).minimize(loss_op)
+#
+#     init = tf.global_variables_initializer()
+#
+#     with tf.Session() as sess:
+#
+#         sess.run(init)
+#
+#         for step in range(epochs):
+#
+#             offset = randint(0, y.shape[0] - batch_size - 1)
+#
+#             batch_data = x[offset:(offset + batch_size), :]
+#             batch_labels = y[offset:(offset + batch_size), :]
+#
+#             feed_dict = {X: batch_data, Y: batch_labels}
+#
+#             _, loss = sess.run([optimizer, loss_op],
+#                                feed_dict=feed_dict)
+#
+#             if step % 1000 == 0:
+#                 pred = tf.nn.softmax(y_pred)
+#                 correct_prediction = tf.equal(tf.argmax(pred, 1), tf.argmax(Y, 1))
+#                 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#                 print('Test accuracy: {}, loss {}'.format(accuracy.eval({X: x_test, Y: y_test}), loss))
+#
+#     print((datetime.now() - start).seconds)
+#     return None
+#
+#
+# def multilayer_perceptron(x, weights, biases):
+#     out_layer = tf.add(tf.matmul(x, weights), biases)
+#     out_layer = tf.nn.sigmoid(out_layer)
+#     return out_layer
 
 
 if __name__ == '__main__':
     home = os.path.expanduser('~')
-    vals = os.path.join(home, 'IrrigationGIS', 'EE_extracts', 'validation_tables', 'validation_12AUG2019.csv')
-    # get_confusion_matrix(vals, ((0, 185), (1, 38), (2, 8740), (3, 1037)))
-    bands = os.path.join(home, 'IrrigationGIS', 'EE_extracts', 'concatenated', 'bands_15JUL_v2_kw_USEDINPAPER.csv')
-    # find_rf_variable_importance(bands)
-    mlp(bands)
+    out_ = os.path.join(home, 'Downloads')
+    extracts = '/media/research/IrrigationGIS/EE_extracts'
+    vals = os.path.join(extracts, 'validation_tables', 'validation_12AUG2019.csv')
+    bands = os.path.join(extracts, 'concatenated', 'bands_15JUL_v2_kw_USEDINPAPER.csv')
+    rf_, names_ = random_forest(bands, binary=False, n_estimators=10)
+    for x in range(10):
+        out_file = os.path.join(out_, 'tree_{}.dot'.format(x))
+        export_tree(rf_, 0, names=names_, out_file=out_file)
+        break
 # ========================= EOF ====================================================================
