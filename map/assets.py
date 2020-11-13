2ac24e9f03947391ed3f7dc8be9f8840aab7bb60
dgketchum <pdWs7kyy>
2020-11-13 15:58:06 -0700

    set time properties without -p flag

--- map/assets.py
+++ map/assets.py
@@ -1,18 +1,3 @@
-# ===============================================================================
-# Copyright 2018 dgketchum
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ===============================================================================
 import csv
 import os
 from subprocess import Popen, PIPE, check_call
@@ -57,10 +42,11 @@ def set_metadata(ee_asset, property='--time_start'):
     reader = list_assets(ee_asset)
     for r in reader:
         year = os.path.basename(r)
-        cmd = ['{}'.format(EXEC), 'asset', 'set', '-p',
-               '(date){}={}-12-31'.format(property, year), r]
-        check_call(cmd)
-        print(cmd)
+        if int(year) > 1986:
+            cmd = ['{}'.format(EXEC), 'asset', 'set',
+                   '{}'.format(property), '{}-12-31T00:00:00'.format(year), r]
+            print(' '.join(cmd))
+            check_call(cmd)
 
 
 def get_metadata(ee_asset):
@@ -122,5 +108,5 @@ def list_assets(location):
 
 if __name__ == '__main__':
     asset = 'projects/ee-dgketchum/assets/IrrMapper/IrrMapper_RF'
-    set_metadata(asset, property='time_end')
+    set_metadata(asset, property='--time_end')
 # ========================= EOF ====================================================================
