5c15483a945a257b79870a2a440df5e6e81a8f2e
dgketchum <pdWs7kyy>
2020-12-11 16:47:03 -0700

    fix cropland extract from CDL

--- map/call_ee.py
+++ map/call_ee.py
@@ -587,9 +587,8 @@ def stack_bands(yr, roi):
     cdl_cult = ee.Image('USDA/NASS/CDL/2017').select('cultivated').remap([1, 2], [0, 1]).reproject(crs=proj['crs'],
                                                                                                    scale=30).rename(
         'cdlclt')
-    cdl_crop = ee.Image('USDA/NASS/CDL/2017').select('cultivated').remap([1, 2], [0, 1]).reproject(crs=proj['crs'],
-                                                                                                   scale=30).rename(
-        'cdlcrp')
+    cdl_crop = ee.Image('USDA/NASS/CDL/2017').select('cropland').reproject(crs=proj['crs'],
+                                                                           scale=30).rename('cdlcrp')
 
     gsw = ee.Image('JRC/GSW1_0/GlobalSurfaceWater')
     occ_pos = gsw.select('occurrence').gt(0)
@@ -630,7 +629,7 @@ def is_authorized():
 
 if __name__ == '__main__':
     is_authorized()
-    for s in ['CO', 'MT', 'CA', 'OR']:
+    for s in ['CA', 'CO', 'ID', 'MT', 'OR', 'WA']:
         shp = '/media/research/IrrigationGIS/EE_extracts/state_point_shp/train/{}/points_10DEC2020.shp'.format(s)
         years_ = count_points(shp)
         pts = os.path.join('projects/ee-dgketchum/assets/points/state', 'pts_{}_10DEC2020'.format(s))
