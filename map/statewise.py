ea84e2bfb748d2ba2b2c58dbcbdce2ceea68aeda
dgketchum <pdWs7kyy>
2021-11-07 13:01:36 -0700

    get bands fro 6NOV2021

--- map/statewise.py
+++ map/statewise.py
@@ -2,6 +2,7 @@ import os
 from subprocess import check_call
 import json
 
+import fiona
 from call_ee import TARGET_STATES, E_STATES, YEARS
 from call_ee import is_authorized, request_band_extract, export_classification
 from tables import concatenate_band_extract
@@ -17,8 +18,8 @@ AEA = '+proj=aea +lat_0=40 +lon_0=-96 +lat_1=20 +lat_2=60 +x_0=0 +y_0=0 +ellps=G
       '+towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 
 
-def to_geographic(in_dir, out_dir):
-    in_shp = [os.path.join(in_dir, x) for x in os.listdir(in_dir) if x.endswith('.shp')]
+def to_geographic(in_dir, out_dir, glob='5NOV2021'):
+    in_shp = [os.path.join(in_dir, x) for x in os.listdir(in_dir) if x.endswith('.shp') and glob in x]
     for s in in_shp:
         out_shp = os.path.join(out_dir, os.path.basename(s))
         cmd = [OGR, '-f', 'ESRI Shapefile', '-t_srs', 'EPSG:4326', '-s_srs', AEA, out_shp, s]
@@ -36,13 +37,16 @@ def push_points_to_asset(glob='5NOV2021'):
         print(id_, s)
 
 
-def get_bands():
+def get_bands(pts_dir, glob='5NOV2021'):
     for s in ALL_STATES:
+        pts = os.path.join(pts_dir, 'points_{}_{}.shp'.format(s, glob))
+        with fiona.open(pts, 'r') as src:
+            years = list(set([x['properties']['YEAR'] for x in src]))
         print('get bands', s)
-        pts = 'users/dgketchum/points/state/points_{}_5NOV2021'.format(s)
+        pts = 'users/dgketchum/points/state/points_{}_{}'.format(s, glob)
         geo = 'users/dgketchum/boundaries/{}'.format(s)
-        file_ = 'bands_{}_5NOV2021'.format(s)
-        request_band_extract(file_, pts, region=geo, years=YEARS, filter_bounds=True)
+        file_ = 'bands_{}_{}'.format(s, glob)
+        request_band_extract(file_, pts, region=geo, years=years, filter_bounds=True)
 
 
 def concatenate_bands(in_dir, out_dir, glob='5NOV2021'):
@@ -96,15 +100,16 @@ def classify(out_coll, variable_dir, tables, years, glob='5NOV2021'):
 
 if __name__ == '__main__':
     is_authorized()
+    _glob = '6NOV2021'
     pt = '/media/research/IrrigationGIS/EE_extracts/point_shp'
     pt_wgs = os.path.join(pt, 'state_wgs')
     pt_aea = os.path.join(pt, 'state_aea')
-    # to_geographic(pt_aea, pt_wgs)
-    # push_points_to_asset()
-    # get_bands()
+    # to_geographic(pt_aea, pt_wgs, glob=_glob)
+    # push_points_to_asset(glob=_glob)
+    get_bands(pt_aea, _glob)
     to_concat = '/media/research/IrrigationGIS/EE_extracts/to_concatenate/state'
     conctenated = '/media/research/IrrigationGIS/EE_extracts/concatenated/state'
-    concatenate_bands(to_concat, conctenated)
+    # concatenate_bands(to_concat, conctenated)
     imp_json = '/media/research/IrrigationGIS/EE_extracts/variable_importance'
     # variable_importance(conctenated, importance_json=None)
     # push_bands_to_asset()
