e1e098a11be4262d22dd634cbe3cd81f9386885d
dgketchum <dgketchum@gmail.com>
2025-08-08 11:53:18 -0600

    training redevelopment running on MLP at 82% acc and UNET on NDVI at 52%.

--- training_redevelopment/redevelop_training_data.py
+++ training_redevelopment/redevelop_training_data.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import time
+import json
 
 import ee
 
@@ -12,6 +13,7 @@ from datetime import datetime
 import fiona
 import geopandas as gpd
 import pandas as pd
+from tqdm import tqdm
 from map.call_ee import is_authorized, stack_bands, BOUNDARIES
 
 sys.setrecursionlimit(5000)
@@ -28,6 +30,8 @@ OGR = '/usr/bin/ogr2ogr'
 AEA = '+proj=aea +lat_0=40 +lon_0=-96 +lat_1=20 +lat_2=60 +x_0=0 +y_0=0 +ellps=GRS80 ' \
       '+towgs84=0,0,0,0,0,0,0 +units=m +no_defs'
 WGS = '+proj=longlat +datum=WGS84 +no_defs'
+
+FEATURE_COLS_DROP = ['system:index', '.geo']
 
 
 def to_geographic(in_dir, out_dir, states, mgrs_path, n_samples=None):
@@ -83,7 +87,8 @@ def push_points_to_asset(_dir, shapefile, bucket):
     print(asset_id, bucket_files[0])
 
 
-def get_bands(shp, southern_states, extract_modern=False, modern_ndvi_dir=None, check_dir=None):
+def get_bands(shp, southern_states, extract_modern=False, modern_ndvi_dir=None, check_dir=None,
+              diagnose=False):
     points_df = gpd.read_file(shp)
 
     if extract_modern:
@@ -107,10 +112,14 @@ def get_bands(shp, southern_states, extract_modern=False, modern_ndvi_dir=None, 
     else:
         file_prefix = 'irrmapper_redev/bands'
 
+    mgrs_ee_tiles = ee.FeatureCollection('users/dgketchum/boundaries/MGRS_TILE')
+
     mgrs_tiles = points_df['MGRS_TILE'].unique()
+
     for tile in mgrs_tiles:
         tile_df = points_df[points_df['MGRS_TILE'] == tile]
         states = tile_df['STUSPS'].unique()
+
         for state in states:
             is_southern = state in southern_states
             state_df = tile_df[tile_df['STUSPS'] == state]
@@ -131,13 +140,38 @@ def get_bands(shp, southern_states, extract_modern=False, modern_ndvi_dir=None, 
 
                 feature_coll = ee.FeatureCollection(year_df.__geo_interface__)
 
-                roi_ = 'users/dgketchum/boundaries/{}'.format(state)
-                region = ee.FeatureCollection(roi_)
+                roi = mgrs_ee_tiles.filterMetadata('MGRS_TILE', 'equals', tile)
+                region = ee.FeatureCollection(roi)
 
-                bands = stack_bands(year, region, southern=False)
+                stack = stack_bands(year, region, southern=False)
+
+                # if tables are coming out empty, use this to find missing bands
+                if diagnose:
+                    filtered = ee.FeatureCollection([feature_coll.first()])
+                    bad_ = []
+                    bands = stack.bandNames().getInfo()
+                    for b in bands:
+                        stack_ = stack.select([b])
+
+                        def sample_regions(i, points):
+                            red = ee.Reducer.toCollection(i.bandNames())
+                            reduced = i.reduceRegions(points, red, 30, stack_.select(b).projection())
+                            fc = reduced.map(lambda f: ee.FeatureCollection(f.get('features'))
+                                             .map(lambda q: q.copyProperties(f, None, ['features'])))
+                            return fc.flatten()
+
+                        data = sample_regions(stack_, filtered)
+                        try:
+                            print(b, data.getInfo()['features'][0]['properties'][b])
+                        except Exception as e:
+                            print(b, 'not there', e)
+                            bad_.append(b)
+                    print(bad_)
+                    return None
+
                 selectors = ['FID', 'POINT_TYPE', 'YEAR', 'MGRS_TILE', 'STUSPS']
 
-                plot_sample_regions = bands.sampleRegions(
+                plot_sample_regions = stack.sampleRegions(
                     collection=feature_coll,
                     properties=selectors,
                     scale=30,
@@ -160,6 +194,61 @@ def get_bands(shp, southern_states, extract_modern=False, modern_ndvi_dir=None, 
                 print(f'{file_prefix}/{desc}')
 
 
+def process_bands_to_parquet(in_dir, out_dir, category_counters=None, overwrite=False):
+
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    file_list = [os.path.join(in_dir, f) for f in os.listdir(in_dir)]
+
+    if category_counters:
+        categories = {c: set() for c in category_counters}
+    else:
+        categories = None
+
+    ct = 0
+    for f in tqdm(file_list):
+        if not f.endswith('.csv'):
+            continue
+
+        try:
+            df = pd.read_csv(f)
+            df.set_index('FID', inplace=True)
+        except pd.errors.EmptyDataError:
+            os.remove(f)
+            continue
+
+        if categories:
+            [categories[c].add(v)  for  c in categories.keys() for v in df[c].unique()]
+
+        for fid, row in df.iterrows():
+            try:
+                year = int(row['YEAR'])
+                pt_type = int(row['POINT_TYPE'])
+                mgrs = row['MGRS_TILE']
+                state = row['STUSPS']
+
+                out_file = os.path.join(out_dir, f'{fid}_{year}_{state}_{mgrs}_{pt_type}.parquet')
+                if os.path.exists(out_file) and not overwrite:
+                    continue
+
+                features = row.drop(['YEAR', 'POINT_TYPE', 'MGRS_TILE', 'STUSPS'])
+
+                features_df = pd.DataFrame(features).T
+                features_df = features_df.drop(columns=FEATURE_COLS_DROP)
+
+                features_df.to_parquet(out_file)
+
+            except Exception as e:
+                print(f"Error processing row {fid} in {f}: {e}")
+
+    if categories:
+        out_json = os.path.join(os.path.dirname(out_dir), 'categorical.json')
+        categories = {k: [int(i) for i in v] for k, v in categories.items()}
+        with open(out_json, 'w') as fp:
+            fp.write(json.dumps(categories, indent=4, sort_keys=True))
+        print(f'Wrote {out_json}')
+
 if __name__ == '__main__':
     is_authorized()
     root = '/media/research/IrrigationGIS/irrmapper'
@@ -170,12 +259,22 @@ if __name__ == '__main__':
     master_shp = os.path.join(pt_wgs, 'master_training_points.shp')
 
     southern = ['AZ', 'CA', 'NM', 'TX', 'OK']
+    #
+    # check_d = '/data/ssd2/irrmapper/bands/past_training/'
+    # get_bands(master_shp, southern, extract_modern=False, check_dir=check_d, diagnose=False)
+    #
+    # check_d = '/data/ssd2/irrmapper/bands/modern_update/'
+    # modern_ndvi_dir_ = '/data/ssd2/irrmapper/ndvi/modern_processed/'
+    # get_bands(master_shp, southern, extract_modern=True, modern_ndvi_dir=modern_ndvi_dir_, check_dir=check_d)
 
-    check_d = '/data/ssd2/irrmapper/bands/past_training/'
-    get_bands(master_shp, southern, extract_modern=False, check_dir=check_d)
+    in_dir_past = '/data/ssd2/irrmapper/bands/past_bands/'
+    out_dir_past = '/data/ssd2/irrmapper/bands/past_processed/'
+    process_bands_to_parquet(in_dir_past, out_dir_past,
+                             category_counters=['nlcd', 'cdl', 'crop5c', 'cropland', 'gsw'],
+                             overwrite=True)
 
-    check_d = '/data/ssd2/irrmapper/bands/modern_update/'
-    modern_ndvi_dir_ = '/data/ssd2/irrmapper/ndvi/modern_processed/'
-    get_bands(master_shp, southern, extract_modern=True, modern_ndvi_dir=modern_ndvi_dir_, check_dir=check_d)
+    # in_dir_modern = '/data/ssd2/irrmapper/bands/modern_bands/'
+    # out_dir_modern = '/data/ssd2/irrmapper/bands/modern_processed/'
+    # process_bands_to_parquet(in_dir_modern, out_dir_modern)
 
 # ========================= EOF ====================================================================
--- training_redevelopment/update_classifier.py
+++ training_redevelopment/update_classifier.py
@@ -4,38 +4,84 @@ from torch.utils.data import Dataset, DataLoader
 import pytorch_lightning as pl
 import pandas as pd
 from sklearn.model_selection import train_test_split
-import numpy as np
 from torchmetrics.classification import Accuracy
+import torch.nn as nn
+import json
 
-from unet import UNet1D
+torch.set_float32_matmul_precision('medium')
 
-class NDVIDataset(Dataset):
-    def __init__(self, file_list):
+CATEGORICAL_FEATURES = ['nlcd', 'cdl']
+
+class MLPWithEmbeddings(nn.Module):
+    def __init__(self, input_dim_continuous, cat_dims, output_dim, emb_dropout=0.1):
+        super(MLPWithEmbeddings, self).__init__()
+        
+        self.embedding_layers = nn.ModuleList([nn.Embedding(num_embeddings, min(50, (num_embeddings + 1) // 2)) for num_embeddings in cat_dims])
+        self.embedding_dropout = nn.Dropout(emb_dropout)
+        
+        embedding_output_size = sum([min(50, (num_embeddings + 1) // 2) for num_embeddings in cat_dims])
+        
+        self.continuous_mlp = nn.Sequential(
+            nn.Linear(input_dim_continuous, 256),
+            nn.ReLU(),
+            nn.BatchNorm1d(256)
+        )
+        
+        self.final_mlp = nn.Sequential(
+            nn.Linear(embedding_output_size + 256, 512),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, output_dim)
+        )
+
+    def forward(self, x_continuous, x_categorical):
+        embedded_features = [emb(x_categorical[:, i]) for i, emb in enumerate(self.embedding_layers)]
+        embedded_features = torch.cat(embedded_features, 1)
+        embedded_features = self.embedding_dropout(embedded_features)
+        
+        continuous_output = self.continuous_mlp(x_continuous)
+        
+        concatenated = torch.cat([continuous_output, embedded_features], 1)
+        return self.final_mlp(concatenated)
+
+class BandsDataset(Dataset):
+    def __init__(self, file_list, all_features, categorical_mappings):
         self.file_list = file_list
+        self.all_features = all_features
+        self.categorical_mappings = categorical_mappings
+        self.continuous_features = [f for f in all_features if f not in CATEGORICAL_FEATURES]
 
     def __len__(self):
         return len(self.file_list)
 
     def __getitem__(self, idx):
         df = pd.read_parquet(self.file_list[idx])
-        df.fillna(0, inplace=True)
-        ndvi = torch.tensor(df.values, dtype=torch.float32).T
+
+        for cat, mapping in self.categorical_mappings.items():
+            df[cat] = df[cat].map(mapping).fillna(0).astype(int)
+
+        continuous_data = torch.tensor(df[self.continuous_features].values, dtype=torch.float32).squeeze()
+        categorical_data = torch.tensor(df[CATEGORICAL_FEATURES].values, dtype=torch.long).squeeze()
+        
         label = int(os.path.basename(self.file_list[idx]).split('_')[-1].split('.')[0])
         if label == 4:
             label = 1
-        return ndvi, torch.tensor(label, dtype=torch.long)
+        return (continuous_data, categorical_data), torch.tensor(label, dtype=torch.long)
 
-class NDVIClassifier(pl.LightningModule):
+class BandsClassifier(pl.LightningModule):
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, input_dim_continuous, cat_dims, output_dim):
         super().__init__()
-        self.model = UNet1D(in_channels, out_channels)
+        self.model = MLPWithEmbeddings(input_dim_continuous, cat_dims, output_dim)
         self.criterion = torch.nn.CrossEntropyLoss()
-        self.val_accuracy = Accuracy(task="multiclass", num_classes=4)
-        self.test_accuracy = Accuracy(task="multiclass", num_classes=4)
+        self.val_accuracy = Accuracy(task="multiclass", num_classes=output_dim)
+        self.test_accuracy = Accuracy(task="multiclass", num_classes=output_dim)
 
     def forward(self, x):
-        return self.model(x)
+        return self.model(x[0], x[1])
 
     def training_step(self, batch, batch_idx):
         x, y = batch
@@ -63,46 +109,56 @@ class NDVIClassifier(pl.LightningModule):
         self.log('test_acc', self.test_accuracy, on_step=False, on_epoch=True, prog_bar=True)
 
     def configure_optimizers(self):
-        return torch.optim.Adam(self.parameters(), lr=1e-3)
+        return torch.optim.Adam(self.parameters(), lr=1e-4)
 
-def train_model(past_processed_dir, modern_processed_dir, batch_size=32, epochs=10):
+def train_model(past_processed_dir, modern_processed_dir, categorical_json_path, batch_size=64, epochs=20):
+
     all_files = [os.path.join(past_processed_dir, f) for f in os.listdir(past_processed_dir) if f.endswith('.parquet')]
+    
+    sample_df = pd.read_parquet(all_files[0])
+    all_feature_names = list(sample_df.columns)
+    continuous_feature_count = len([f for f in all_feature_names if f not in CATEGORICAL_FEATURES])
+    
+    with open(categorical_json_path, 'r') as f:
+        categorical_data = json.load(f)
+
+    categorical_mappings = {}
+    categorical_dims = []
+    for cat in CATEGORICAL_FEATURES:
+        unique_values = sorted(categorical_data[cat])
+        mapping = {val: i for i, val in enumerate(unique_values)}
+        categorical_mappings[cat] = mapping
+        categorical_dims.append(len(unique_values))
+
     train_files, test_files = train_test_split(all_files, test_size=0.2, random_state=42)
     train_files, val_files = train_test_split(train_files, test_size=0.2, random_state=42)
 
-    with open('train_test_split.txt', 'w') as f:
-        f.write('Training Files:\n')
-        for item in train_files:
-            f.write("%s\n" % item)
-        f.write('\nValidation Files:\n')
-        for item in val_files:
-            f.write("%s\n" % item)
-        f.write('\nTest Files:\n')
-        for item in test_files:
-            f.write("%s\n" % item)
+    train_dataset = BandsDataset(train_files, all_feature_names, categorical_mappings)
 
-    train_dataset = NDVIDataset(train_files)
-    val_dataset = NDVIDataset(val_files)
-    test_dataset = NDVIDataset(test_files)
+    train_dataset.__getitem__(0)
 
-    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=4)
-    val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=4)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=4)
+    val_dataset = BandsDataset(val_files, all_feature_names, categorical_mappings)
+    test_dataset = BandsDataset(test_files, all_feature_names, categorical_mappings)
 
-    model = NDVIClassifier(in_channels=1, out_channels=4)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=8)
+    val_loader = DataLoader(val_dataset, batch_size=batch_size, num_workers=8)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, num_workers=8)
+
+    model = BandsClassifier(input_dim_continuous=continuous_feature_count, cat_dims=categorical_dims, output_dim=4)
 
     trainer = pl.Trainer(max_epochs=epochs, accelerator='gpu', devices=1)
     trainer.fit(model, train_loader, val_loader)
     trainer.test(model, dataloaders=test_loader)
 
-    predict_modern_data(model, modern_processed_dir, batch_size)
+    predict_modern_data(model, modern_processed_dir, batch_size, all_feature_names, categorical_mappings)
 
-def predict_modern_data(model, modern_processed_dir, batch_size):
-    modern_dataset = NDVIDataset([os.path.join(modern_processed_dir, f) for f in
-                                  os.listdir(modern_processed_dir) if f.endswith('.parquet')])
-    modern_loader = DataLoader(modern_dataset, batch_size=batch_size, num_workers=4)
+def predict_modern_data(model, modern_processed_dir, batch_size, all_features, categorical_mappings):
+    modern_dataset = BandsDataset([os.path.join(modern_processed_dir, f) for f in
+                                  os.listdir(modern_processed_dir) if f.endswith('.parquet')], all_features, categorical_mappings)
+    modern_loader = DataLoader(modern_dataset, batch_size=batch_size, num_workers=8)
 
     predictions = []
+    model.eval()
     with torch.no_grad():
         for batch in modern_loader:
             x, _ = batch
@@ -112,12 +168,15 @@ def predict_modern_data(model, modern_processed_dir, batch_size):
 
     fids = [os.path.basename(f).split('_')[0] for f in modern_dataset.file_list]
     results = pd.DataFrame({'FID': fids, 'predicted_class': predictions})
-    results.to_csv('modern_predictions.csv', index=False)
-    print("Modern data predictions saved to modern_predictions.csv")
+    results.to_csv('modern_bands_predictions_with_embeddings.csv', index=False)
+    print("Modern data predictions saved to modern_bands_predictions_with_embeddings.csv")
 
 if __name__ == '__main__':
-    past_dir = '/data/ssd2/irrmapper/ndvi/past_processed/'
-    modern_dir = '/data/ssd2/irrmapper/ndvi/modern_processed/'
-    train_model(past_dir, modern_dir)
+    past_dir = '/data/ssd2/irrmapper/bands/past_processed/'
+    modern_dir = '/data/ssd2/irrmapper/bands/modern_processed/'
+
+    categorical_map = '/data/ssd2/irrmapper/bands/past_processed/categorical.json'
+
+    train_model(past_dir, modern_dir, categorical_map)
 
 # ========================= EOF ====================================================================
