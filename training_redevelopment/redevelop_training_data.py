c4b5b3fb1238c35803bcac20e2e7a6bde185573e
dgketchum <dgketchum@gmail.com>
2025-08-08 14:36:55 -0600

    state-by_state mgrs based extract on bands

--- training_redevelopment/redevelop_training_data.py
+++ training_redevelopment/redevelop_training_data.py
@@ -8,9 +8,8 @@ import ee
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import os
 from subprocess import check_call
-from datetime import datetime
+import random
 
-import fiona
 import geopandas as gpd
 import pandas as pd
 from tqdm import tqdm
@@ -22,8 +21,8 @@ home = os.path.expanduser('~')
 conda = os.path.join(home, 'miniconda3', 'envs')
 if not os.path.exists(conda):
     conda = conda.replace('miniconda3', 'miniconda')
-EE = '/home/dgketchum/miniconda3/envs/met/bin/earthengine'
-GS = '/home/dgketchum/google-cloud-sdk/bin/gsutil'
+EE = '/home/dgketchum/miniconda/envs/irmp/bin/earthengine'
+GS = '/home/dgketchum/miniconda/envs/irmp/bin/gsutil'
 
 OGR = '/usr/bin/ogr2ogr'
 
@@ -33,81 +32,79 @@ WGS = '+proj=longlat +datum=WGS84 +no_defs'
 
 FEATURE_COLS_DROP = ['system:index', '.geo']
 
+TRAINING_DATA = {'AZ': 'AZ_24NOV2021', 'CA': 'CA_14NOV2021',
+                 'CO': 'CO_10NOV2021', 'ID': 'ID_10NOV2021',
+                 'KS': 'KS_7NOV2021', 'MT': 'MT_15NOV2021',
+                 'ND': 'ND_7NOV2021', 'NE': 'NE_7NOV2021',
+                 'NM': 'NM_7NOV2021', 'NV': 'NV_7NOV2021',
+                 'OK': 'OK_7NOV2021', 'OR': 'OR_22NOV2021',
+                 'SD': 'SD_7NOV2021', 'TX': 'TX_7NOV2021',
+                 'UT': 'UT_10NOV2021', 'WA': 'WA_10NOV2021',
+                 'WY': 'WY_7NOV2021'}
+
 
 def to_geographic(in_dir, out_dir, states, mgrs_path, n_samples=None):
-    df_list = []
+    """Creates a shapefile for each state with MGRS tile info."""
+    out_shapefiles = []
     for state in states:
-        in_shp = [os.path.join(in_dir, x) for x in os.listdir(in_dir) if x.endswith('.shp') and state in x]
-        dates = []
-        for s in in_shp:
-            try:
-                d = datetime.strptime(s.split('_')[-1].split('.')[0], '%d%b%Y')
-                dates.append(d)
-            except ValueError:
-                continue
-        latest = in_shp[dates.index(max(dates))]
+        print(f"Processing points for {state}...")
+        in_shp = [os.path.join(in_dir, x) for x in os.listdir(in_dir) if
+                  x.endswith('.shp') and TRAINING_DATA[state] in x][0]
         mgrs = gpd.read_file(mgrs_path)
 
-        points = gpd.read_file(latest)
+        points = gpd.read_file(in_shp)
         points = points.sjoin(mgrs, how="inner")
         points = points.to_crs(epsg=4326)
         points.drop(columns=['index_right'], inplace=True)
         points['STUSPS'] = state
+        points['NEW_YEAR'] = [random.randint(2017, 2024) for _ in range(len(points))]
 
         if n_samples:
             points = points.groupby('POINT_TYPE', group_keys=False).apply(lambda x: x.sample(n=n_samples))
-            points.index = list(range(points.shape[0]))
 
-        df_list.append(points)
+        points.index = range(len(points))
+        points['FID'] = [f'{row['STUSPS']}_{i:06d}' for i, row in points.iterrows()]
 
-    df = pd.concat(df_list, ignore_index=True)
-    df['FID'] = ['%s_%s' % (row['STUSPS'], str(i).zfill(6)) for i, row in df.iterrows()]
+        out_shp = os.path.join(out_dir, f'{TRAINING_DATA[state]}.shp')
+        points.to_file(out_shp)
+        out_shapefiles.append(out_shp)
+        print(f"Wrote {out_shp}")
 
-    out_shp = os.path.join(out_dir, 'master_training_points.shp')
-    df.to_file(out_shp)
-    print(out_shp)
-    return out_shp
+    return out_shapefiles
 
 
 def push_points_to_asset(_dir, shapefile, bucket):
+    """Uploads a single state's shapefile to a unique Earth Engine asset."""
     shp_name = os.path.basename(shapefile).replace('.shp', '')
-    local_files = [os.path.join(_dir, '{}.{}'.format(shp_name, ext)) for ext in
-                   ['shp', 'prj', 'shx', 'dbf']]
-    bucket = os.path.join(bucket, 'state_points')
-    bucket_files = [os.path.join(bucket, '{}.{}'.format(shp_name, ext)) for ext in
-                    ['shp', 'prj', 'shx', 'dbf']]
-    for lf, bf in zip(local_files, bucket_files):
-        cmd = [GS, 'cp', lf, bf]
-        check_call(cmd)
+    asset_id = f"users/dgketchum/points/state_mgrs/{shp_name}"
 
-    asset_id = os.path.basename(bucket_files[0]).split('.')[0]
-    ee_dst = 'users/dgketchum/points/state_redev/{}'.format(asset_id)
-    cmd = [EE, 'upload', 'table', '-f', '--asset_id={}'.format(ee_dst), bucket_files[0]]
+    print(f"Uploading {shp_name} to GCS...")
+    gcs_path = os.path.join(bucket, 'redevelopment_points', os.path.basename(shapefile))
+    cmd = [GS, 'cp', shapefile, gcs_path]
     check_call(cmd)
-    print(asset_id, bucket_files[0])
+
+    for ext in ['prj', 'shx', 'dbf', 'cpg']:
+        sidecar_file = shapefile.replace('.shp', f'.{ext}')
+        if os.path.exists(sidecar_file):
+            cmd = [GS, 'cp', sidecar_file, os.path.join(bucket, 'redevelopment_points')]
+            check_call(cmd)
+
+    print(f"Uploading {gcs_path} to Earth Engine asset {asset_id}")
+    cmd = [EE, 'upload', 'table', '-f', f'--asset_id={asset_id}', gcs_path]
+    check_call(cmd)
+    print(f"Upload complete for {asset_id}")
+    return asset_id
 
 
-def get_bands(shp, southern_states, extract_modern=False, modern_ndvi_dir=None, check_dir=None,
+def get_bands(shp_dir, extract_modern=False, modern_ndvi_dir=None, check_dir=None,
               diagnose=False):
-    points_df = gpd.read_file(shp)
+
+    shapefiles = [os.path.join(shp_dir, f) for f in os.listdir(shp_dir) if f.endswith('.shp')]
+    points_dfs = [gpd.read_file(shp) for shp in shapefiles]
+    points_df = pd.concat(points_dfs)
 
     if extract_modern:
-        if not modern_ndvi_dir:
-            raise ValueError("modern_ndvi_dir must be provided when extract_modern is True")
-
         file_prefix = 'irrmapper_redev/bands_modern'
-
-        print("Extracting modern years from processed NDVI files...")
-        modern_files = [f for f in os.listdir(modern_ndvi_dir) if f.endswith('.parquet')]
-        modern_info = []
-        for f in modern_files:
-            parts = os.path.basename(f).split('_')
-            fid = '_'.join(parts[:2])
-            year = int(parts[2])
-            modern_info.append({'FID': fid, 'YEAR': year})
-
-        modern_years_df = pd.DataFrame(modern_info)
-        points_df = points_df.drop(columns=['YEAR']).merge(modern_years_df, on='FID')
 
     else:
         file_prefix = 'irrmapper_redev/bands'
@@ -116,22 +113,30 @@ def get_bands(shp, southern_states, extract_modern=False, modern_ndvi_dir=None, 
 
     mgrs_tiles = points_df['MGRS_TILE'].unique()
 
-    for tile in mgrs_tiles:
-        tile_df = points_df[points_df['MGRS_TILE'] == tile]
-        states = tile_df['STUSPS'].unique()
+    states = points_df['STUSPS'].unique()
 
-        for state in states:
-            is_southern = state in southern_states
-            state_df = tile_df[tile_df['STUSPS'] == state]
-            years = sorted(list(state_df['YEAR'].unique()))
+    for state in states:
+        state_df = points_df[points_df['STUSPS'] == state]
+
+        for tile in mgrs_tiles:
+            tile_df = state_df[state_df['MGRS_TILE'] == tile]
+
+            if modern_ndvi_dir:
+                years = sorted(list(tile_df['NEW_YEAR'].unique()))
+            else:
+                years = sorted(list(tile_df['YEAR'].unique()))
 
             for year in years:
-                year_df = state_df[state_df['YEAR'] == year]
+                year_df = tile_df[tile_df['YEAR'] == year]
 
                 if year_df.empty:
                     continue
 
                 desc = f'bands_{tile}_{state}_{year}'
+
+                # densest extract
+                if desc != f'bands_12TUL_UT_2009':
+                    continue
 
                 if check_dir:
                     if os.path.exists(os.path.join(check_dir, f'{desc}.csv')):
@@ -195,7 +200,6 @@ def get_bands(shp, southern_states, extract_modern=False, modern_ndvi_dir=None, 
 
 
 def process_bands_to_parquet(in_dir, out_dir, category_counters=None, overwrite=False):
-
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
 
@@ -219,7 +223,7 @@ def process_bands_to_parquet(in_dir, out_dir, category_counters=None, overwrite=
             continue
 
         if categories:
-            [categories[c].add(v)  for  c in categories.keys() for v in df[c].unique()]
+            [categories[c].add(v) for c in categories.keys() for v in df[c].unique()]
 
         for fid, row in df.iterrows():
             try:
@@ -249,32 +253,29 @@ def process_bands_to_parquet(in_dir, out_dir, category_counters=None, overwrite=
             fp.write(json.dumps(categories, indent=4, sort_keys=True))
         print(f'Wrote {out_json}')
 
+
 if __name__ == '__main__':
     is_authorized()
-    root = '/media/research/IrrigationGIS/irrmapper'
+    root = '/media/research/IrrigationGIS'
     if not os.path.exists(root):
-        root = '/home/dgketchum/data/IrrigationGIS/irrmapper'
+        root = '/home/dgketchum/data/IrrigationGIS'
 
-    pt_wgs = os.path.join(root, 'EE_extracts/point_shp', 'state_wgs_mgrs')
-    master_shp = os.path.join(pt_wgs, 'master_training_points.shp')
+    pt_aea = os.path.join(root, 'irrmapper', 'EE_extracts', 'point_shp', 'state_aea')
+    pt_wgs = os.path.join(root, 'irrmapper', 'EE_extracts', 'point_shp', 'state_wgs_mgrs')
+    mgrs = os.path.join(root, 'boundaries', 'mgrs', 'mgrs_aea.shp')
+    bucket = 'gs://wudr'
 
-    southern = ['AZ', 'CA', 'NM', 'TX', 'OK']
-    #
-    # check_d = '/data/ssd2/irrmapper/bands/past_training/'
-    # get_bands(master_shp, southern, extract_modern=False, check_dir=check_d, diagnose=False)
-    #
-    # check_d = '/data/ssd2/irrmapper/bands/modern_update/'
-    # modern_ndvi_dir_ = '/data/ssd2/irrmapper/ndvi/modern_processed/'
-    # get_bands(master_shp, southern, extract_modern=True, modern_ndvi_dir=modern_ndvi_dir_, check_dir=check_d)
+    states = ['AZ', 'CA', 'CO', 'ID', 'MT', 'NM', 'NV', 'OR', 'UT', 'WA', 'WY']
 
-    in_dir_past = '/data/ssd2/irrmapper/bands/past_bands/'
-    out_dir_past = '/data/ssd2/irrmapper/bands/past_processed/'
-    process_bands_to_parquet(in_dir_past, out_dir_past,
-                             category_counters=['nlcd', 'cdl', 'crop5c', 'cropland', 'gsw'],
-                             overwrite=True)
+    # state_shapefiles = to_geographic(pt_aea, pt_wgs, states=states, mgrs_path=mgrs)
 
-    # in_dir_modern = '/data/ssd2/irrmapper/bands/modern_bands/'
-    # out_dir_modern = '/data/ssd2/irrmapper/bands/modern_processed/'
-    # process_bands_to_parquet(in_dir_modern, out_dir_modern)
+    # for shp in state_shapefiles:
+        # push_points_to_asset(pt_wgs, shp, bucket)
+
+    chk = '/data/ssd2/irrmapper/bands/states/bands_past/'
+    get_bands(pt_wgs, extract_modern=False, check_dir=chk, diagnose=False)
+
+    chk = '/data/ssd2/irrmapper/bands/states/bands_modern/'
+    get_bands(pt_wgs, extract_modern=True, check_dir=chk, diagnose=False)
 
 # ========================= EOF ====================================================================
